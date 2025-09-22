72bf3b0fe693dcb23b6c77f33049f9994beacf45
klucianob <kevobuyan@gmail.com>
2025-09-22 22:46:32 +0300

    zipstring

---
+++ no4/zipstring.go
@@ -0,0 +1,36 @@
+/*
+zipstring
+Instructions
+Write a function that takes a string and returns a new string that replaces every character with the number of
+duplicates and the character itself, deleting the extra duplications.
+
+The letters are from the latin alphabet list only. Any other character, symbols, shall not be tested.
+Expected function
+*/
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func ZipString(s string) string {
+	count := 1
+	result := ""
+	for i := 1; i < len(s); i++ {
+
+		if i < len(s) && (s[i] == s[i-1]) {
+
+			count++
+		} else {
+			result += strconv.Itoa(count) + string(s[i-1])
+			count = 1
+		}
+	}
+	return result
+}
+func main() {
+	fmt.Println(ZipString("YouuungFellllas"))
+	fmt.Println(ZipString("Thee quuick browwn fox juumps over the laaazy dog"))
+	fmt.Println(ZipString("Helloo Therre!"))
+}
